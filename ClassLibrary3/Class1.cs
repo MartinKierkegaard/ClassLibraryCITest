cd491e0031128fdadcd7e2af06b124ec7db3107f
Martin Kierkegaard <mark@easj.dk>
2018-03-05 12:25:07 +0100

    Add project files.

---
+++ ClassLibrary3/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibrary3
+{
+    public class Class1
+    {
+        public string GetName()
+        {
+            return "Martin";
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ClassLibrary3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 c = new Class1();
+
+            var result = c.GetName();
+
+            Assert.AreEqual("Martin", result);
+
+        }
+    }
+}
